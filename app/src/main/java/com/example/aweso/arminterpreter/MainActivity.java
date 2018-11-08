2f330c68a20de7b6d94424d52a7d61a4f1b09559
charlie <charlie.tran@cuw.edu>
2018-11-08 12:55:20 -0600

    Changes to add value and execute

--- app/src/main/java/com/example/aweso/arminterpreter/MainActivity.java
+++ app/src/main/java/com/example/aweso/arminterpreter/MainActivity.java
@@ -4,13 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText instructionET;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.instructionET = (EditText)this.findViewById(R.id.instructionET);
         ARMap.init();
         //ARMap.lookupInstruction("ADD").display();
     }
@@ -28,5 +30,17 @@ public class MainActivity extends AppCompatActivity {
         //For example, if instructionET contained: ADD X0, X1, X2
         //Your code should grab the value from X1 and X2, add them
         //together, and store the result in X0
+        String instruction = this.instructionET.getText().toString();
+        String instructionName = instruction.substring(0, 3);
+        String instructionReg = instruction.substring(3);
+        String[] seperated = instructionReg.split(",");
+        for(int i = 0; i < ARMap.registers.length; i++)
+        {
+            if(ARMap.registers[i] == 
+        }
+        if(instruction == "ADD")
+        {
+            seperated[0] = seperated[2] + seperated[1];
+        }
     }
 }
